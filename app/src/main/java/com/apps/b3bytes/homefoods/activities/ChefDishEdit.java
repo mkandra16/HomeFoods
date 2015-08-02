ba8bcf6ea3008166b19e406f597736d72c7ffb37
b3bytes <b3bytes@users.noreply.github.com>
2015-08-02 09:15:02 -0700

    [Chef Dish Edit] add date dialog fragment

--- app/src/main/java/com/apps/b3bytes/homefoods/activities/ChefDishEdit.java
+++ app/src/main/java/com/apps/b3bytes/homefoods/activities/ChefDishEdit.java
@@ -1,15 +1,31 @@
 package com.apps.b3bytes.homefoods.activities;
 
+import android.app.DatePickerDialog;
+import android.app.FragmentTransaction;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.apps.b3bytes.homefoods.R;
+import com.apps.b3bytes.homefoods.fragments.DatePickerDialogFragment;
 
-public class ChefDishEdit extends ActionBarActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class ChefDishEdit extends ActionBarActivity implements DatePickerDialog.OnDateSetListener {
+    private RelativeLayout lDishEditFromDatePicker;
+    private RelativeLayout lDishEditToDatePicker;
+    private int datePickerInput;
+    public static final int DATE_DIALOG_FRAGMENT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +37,51 @@ public class ChefDishEdit extends ActionBarActivity {
                 this, R.array.cuisine_picker_array, R.layout.spinner_layout);
         foodadapter.setDropDownViewResource(R.layout.spinner_layout);
         food.setAdapter(foodadapter);
+
+        lDishEditFromDatePicker = (RelativeLayout) findViewById(R.id.lDishEditFromDatePicker);
+        TextView tvDishEditFromDateHdr = (TextView) lDishEditFromDatePicker.findViewById(R.id.tvDishEditDateHdr);
+        tvDishEditFromDateHdr.setText("AVILABLE FROM");
+        TextView tvDishEditFromDatePicker = (TextView) lDishEditFromDatePicker.findViewById(R.id.tvDishEditDatePicker);
+
+        tvDishEditFromDatePicker.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                datePickerInput = lDishEditFromDatePicker.getId();
+                DialogFragment newFragment = new DatePickerDialogFragment();
+                newFragment.show(getSupportFragmentManager(), "From");
+            }
+        });
+
+        lDishEditToDatePicker = (RelativeLayout) findViewById(R.id.lDishEditToDatePicker);
+        TextView tvDishEditToDateHdr = (TextView) lDishEditToDatePicker.findViewById(R.id.tvDishEditDateHdr);
+        tvDishEditToDateHdr.setText("TO");
+        TextView tvDishEditToDatePicker = (TextView) lDishEditToDatePicker.findViewById(R.id.tvDishEditDatePicker);
+        tvDishEditToDatePicker.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                datePickerInput = lDishEditToDatePicker.getId();
+                DialogFragment newFragment = new DatePickerDialogFragment();
+                newFragment.show(getSupportFragmentManager(), "To");
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear,
+                          int dayOfMonth) {
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        cal.set(Calendar.MONTH, monthOfYear);
+        String formattedDate = new SimpleDateFormat("E, MMM d, yyyy").format(cal.getTime());
+
+        if (datePickerInput == lDishEditFromDatePicker.getId())
+            ((TextView) lDishEditFromDatePicker.findViewById(R.id.tvDishEditDatePicker)).setText(formattedDate);
+        else if (datePickerInput == lDishEditToDatePicker.getId())
+            ((TextView) lDishEditToDatePicker.findViewById(R.id.tvDishEditDatePicker)).setText(formattedDate);
 
     }
 
