7161f6ee5d3fcc6848d19f824eb590f7381355bf
Pavan <patibandla@gmail.com>
2015-09-02 08:30:15 -0700

    Removed dead code around order delivery.

--- app/src/main/java/com/apps/b3bytes/homefoods/activities/WelcomeScreen.java
+++ app/src/main/java/com/apps/b3bytes/homefoods/activities/WelcomeScreen.java
@@ -49,18 +49,6 @@ public class WelcomeScreen extends ActionBarActivity {
         // This is first Screen, initialize global state of the App.
         AppGlobalState.initialize(getApplicationContext());
 
-        bChefDeliveryScreenNavigate = (Button) findViewById(R.id.bChefDeliveryScreenNavigate);
-
-        bChefDeliveryScreenNavigate.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-
-                Intent i = new Intent(WelcomeScreen.this, ChefOrderDelivery.class);
-
-                WelcomeScreen.this.startActivity(i);
-            }
-        });
-
         bHomePageScreenNavigate = (Button) findViewById(R.id.bHomePageScreenNavigate);
 
         bHomePageScreenNavigate.setOnClickListener(new View.OnClickListener() {
